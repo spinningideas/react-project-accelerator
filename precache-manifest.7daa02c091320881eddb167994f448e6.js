cf4b2197267ac12fd7a65df489af7e447111f9af
spinningideas <scott@spinningideas.com>
2020-05-25 19:22:02 -0600

    Updates

---
+++ precache-manifest.7daa02c091320881eddb167994f448e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdadb3316e60b2c03e6e8930567b8fb",
+    "url": "https://spinningideas.github.io/react-project-accelerator/index.html"
+  },
+  {
+    "revision": "8d070cbb3d3778ee8d22",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/css/main.84388eb8.chunk.css"
+  },
+  {
+    "revision": "4233f44f43352a79803e",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/js/2.fd972722.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/js/2.fd972722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c984c04b7d81cdb5cb4b",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/js/3.6236dd04.chunk.js"
+  },
+  {
+    "revision": "01cd4b73a178680234b1",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/js/4.79f690d3.chunk.js"
+  },
+  {
+    "revision": "9be44188068b88b14cb2",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/js/5.a5dbfe92.chunk.js"
+  },
+  {
+    "revision": "8d070cbb3d3778ee8d22",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/js/main.549e62c4.chunk.js"
+  },
+  {
+    "revision": "c3b93da2f49ee503644e",
+    "url": "https://spinningideas.github.io/react-project-accelerator/static/js/runtime-main.ba54f6a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://github.com/spinningideas/react-project-accelerator/precache-manifest.57068d2584d2fc7874ccc3436c0e0625.js"
+  "https://spinningideas.github.io/react-project-accelerator/precache-manifest.7daa02c091320881eddb167994f448e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://github.com/spinningideas/react-project-accelerator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://spinningideas.github.io/react-project-accelerator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
