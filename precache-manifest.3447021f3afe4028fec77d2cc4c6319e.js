d9e7fbd03f88755d2f4a4f6b969bd47ef5088d05
spinningideas <scott@spinningideas.com>
2020-05-25 18:54:11 -0600

    Updates

---
+++ precache-manifest.3447021f3afe4028fec77d2cc4c6319e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125b49ee78aab145ea23744e6ce573a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "755ac43a8171d562d8fd",
+    "url": "/static/css/main.84388eb8.chunk.css"
+  },
+  {
+    "revision": "4233f44f43352a79803e",
+    "url": "/static/js/2.fd972722.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fd972722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c984c04b7d81cdb5cb4b",
+    "url": "/static/js/3.6236dd04.chunk.js"
+  },
+  {
+    "revision": "01cd4b73a178680234b1",
+    "url": "/static/js/4.79f690d3.chunk.js"
+  },
+  {
+    "revision": "9be44188068b88b14cb2",
+    "url": "/static/js/5.a5dbfe92.chunk.js"
+  },
+  {
+    "revision": "755ac43a8171d562d8fd",
+    "url": "/static/js/main.2dd73a8d.chunk.js"
+  },
+  {
+    "revision": "fa43fcb34502ebc966f7",
+    "url": "/static/js/runtime-main.a28a28fe.js"
+  }
+]);
