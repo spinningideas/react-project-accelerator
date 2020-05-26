8378c3e859226a69458c266cb43ed1f66cd92693
spinningideas <scott@spinningideas.com>
2020-05-26 13:43:32 -0600

    tabs fix

--- src/routes.js
+++ src/routes.js
@@ -4,22 +4,15 @@ import Loadable from 'react-loadable';
 import LoadingIndicator from 'components/LoadingIndicator';
 
 const routes = (
-	<Switch>
-		<Route
-			path="/"
-			exact
-			component={Loadable({ loader: () => import('./pages/Home'), loading: LoadingIndicator })}
-		/>
-		<Route
-			path="/about"
-			component={Loadable({ loader: () => import('./pages/About'), loading: LoadingIndicator })}
-		/>
-		<Route
-			path="/contact"
-			component={Loadable({ loader: () => import('./pages/Contact'), loading: LoadingIndicator })}
-		/>
-		<Redirect to="/" />
-	</Switch>
+  <Switch>
+    <Route path="/" exact component={Loadable({ loader: () => import('./pages/Home'), loading: LoadingIndicator })} />
+    <Route path="/about" component={Loadable({ loader: () => import('./pages/About'), loading: LoadingIndicator })} />
+    <Route
+      path="/contact"
+      component={Loadable({ loader: () => import('./pages/Contact'), loading: LoadingIndicator })}
+    />
+    <Redirect to="/" />
+  </Switch>
 );
 
 export default routes;
