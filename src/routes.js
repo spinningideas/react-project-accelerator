31408813ee145fe4690b9b14af5102cf0d6879fc
spinningideas <scott@spinningideas.com>
2020-07-11 16:22:08 -0600

    formatting fix

--- src/routes.js
+++ src/routes.js
@@ -3,20 +3,19 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import LoadingIndicator from 'components/Shared/LoadingIndicator';
 
 const LazyLoadedPage = (pageName) => {
-	return lazy(() => import(`./pages/${pageName}`))
+  return lazy(() => import(`./pages/${pageName}`));
 };
 
-
 const routes = (
-	<Suspense fallback={<LoadingIndicator />}>
-		<Switch>
-			<Route path="/" exact component={LazyLoadedPage('Home')} />
-			<Route path="/about" component={LazyLoadedPage('About')}  />
-			<Route path="/contact/:name" component={LazyLoadedPage('Contact')} />
-			<Route path="/contact" component={LazyLoadedPage('Contact')}  />
-			<Redirect to="/" />
-		</Switch>
-	</Suspense>
+  <Suspense fallback={<LoadingIndicator />}>
+    <Switch>
+      <Route path="/" exact component={LazyLoadedPage('Home')} />
+      <Route path="/about" component={LazyLoadedPage('About')} />
+      <Route path="/contact/:name" component={LazyLoadedPage('Contact')} />
+      <Route path="/contact" component={LazyLoadedPage('Contact')} />
+      <Redirect to="/" />
+    </Switch>
+  </Suspense>
 );
 
 export default routes;
