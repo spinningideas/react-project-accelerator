371781152f3cca44a144f07fbe1af11879da97c5
spinningideas <scott@spinningideas.com>
2020-07-11 16:18:22 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://spinningideas.github.io/react-project-accelerator/precache-manifest.9a6207a335ea1f1c310f9556586459d9.js"
+  "https://spinningideas.github.io/react-project-accelerator/precache-manifest.6d4b4aba1fe35833343dddef27a30605.js"
 );
 
 self.addEventListener('message', (event) => {
