6ed18609120ae62976b431b8e182e5eb374d9edb
Spinning Idea <github@spinningideas.com>
2026-01-28 12:50:44 -0500

    Updates

---
+++ assets/NotificationsService-BnA2iRyN.js
@@ -0,0 +1,2 @@
+import{ai as r}from"./index-C8F7rr-e.js";const t=()=>({show:(o,s="info")=>{switch(s){case"success":r.success(o);break;case"error":r.error(o);break;default:r.info(o);break}}});export{t as N};
+//# sourceMappingURL=NotificationsService-BnA2iRyN.js.map
