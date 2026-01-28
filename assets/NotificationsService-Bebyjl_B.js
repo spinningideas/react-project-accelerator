b30a5e64f9c3235e16e4e2addb6e8e4c41641a1c
Spinning Idea <github@spinningideas.com>
2026-01-27 19:04:07 -0500

    Updates

---
+++ assets/NotificationsService-Bebyjl_B.js
@@ -0,0 +1,1 @@
+import{ag as r}from"./index-B6osot6O.js";const t=()=>({show:(o,s="info")=>{switch(s){case"success":r.success(o);break;case"error":r.error(o);break;default:r.info(o);break}}});export{t as N};
