7c8629cc4772740832596e32cb35bbe7ded77622
Spinning Idea <github@spinningideas.com>
2026-01-27 23:13:43 -0500

    Updates

--- assets/NotificationsService-BpT10gg0.js
+++
@@ -1,1 +0,0 @@
-import{ai as r}from"./index-BOSc5o6q.js";const t=()=>({show:(o,s="info")=>{switch(s){case"success":r.success(o);break;case"error":r.error(o);break;default:r.info(o);break}}});export{t as N};
