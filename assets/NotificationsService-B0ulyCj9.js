d4ea5fb16f25d93a4b52b50bff777fb1686a6d22
Spinning Idea <github@spinningideas.com>
2026-01-27 19:38:28 -0500

    Updates

---
+++ assets/NotificationsService-B0ulyCj9.js
@@ -0,0 +1,1 @@
+import{ag as r}from"./index-aIcObG6s.js";const t=()=>({show:(o,s="info")=>{switch(s){case"success":r.success(o);break;case"error":r.error(o);break;default:r.info(o);break}}});export{t as N};
