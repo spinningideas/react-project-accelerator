8539d17ddd86b265e35898b0951e8166f9676169
Spinning Idea <github@spinningideas.com>
2026-01-27 19:33:31 -0500

    Updates

---
+++ assets/NotificationsService-CXRxd20H.js
@@ -0,0 +1,1 @@
+import{ag as r}from"./index-CBnQef75.js";const t=()=>({show:(o,s="info")=>{switch(s){case"success":r.success(o);break;case"error":r.error(o);break;default:r.info(o);break}}});export{t as N};
