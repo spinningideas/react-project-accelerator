8c6ac5754f7cfea8905a7dd3fdf3f7ceb92abc35
Spinning Idea <github@spinningideas.com>
2026-02-03 15:09:10 -0500

    Updates

---
+++ assets/NotificationsService-BcsJAUeP.js
@@ -0,0 +1,2 @@
+import{x as e}from"./index-BKCIwwMb.js";const n=()=>{const{toastSuccess:t,toastError:s,toastInfo:a,showToast:r}=e();return{show:(o,c="info")=>{switch(c){case"success":t(o);break;case"error":s(o);break;case"info":a(o);break;default:r(o,"default");break}}}};export{n as N};
+//# sourceMappingURL=NotificationsService-BcsJAUeP.js.map
