43b5488a404a298be9acc65fd6579869bc6eb3fd
Spinning Idea <github@spinningideas.com>
2026-02-03 13:20:46 -0500

    Updates

--- assets/NotificationsService-BK5yqJKF.js
+++
@@ -1,2 +0,0 @@
-import{ah as t}from"./index-CftBfrJG.js";const n=()=>{const{success:s,error:r,info:a,showToast:c}=t();return{show:(o,e="info")=>{switch(e){case"success":s(o);break;case"error":r(o);break;case"info":a(o);break;default:c(o,"default");break}}}};export{n as N};
-//# sourceMappingURL=NotificationsService-BK5yqJKF.js.map
---
+++ assets/NotificationsService-BL1yF89s.js
@@ -0,0 +1,2 @@
+import{ah as t}from"./index-AanaJUsu.js";const n=()=>{const{success:s,error:r,info:a,showToast:c}=t();return{show:(o,e="info")=>{switch(e){case"success":s(o);break;case"error":r(o);break;case"info":a(o);break;default:c(o,"default");break}}}};export{n as N};
+//# sourceMappingURL=NotificationsService-BL1yF89s.js.map
