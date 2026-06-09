12bf034237537864aef3cd0bee3e783b5cdb6a99
Spinning Idea <github@spinningideas.com>
2026-06-09 12:22:38 -0400

    Updates

--- assets/NotificationsService-6L-rsrZK.js
+++
@@ -1,2 +0,0 @@
-import{w as e}from"./index-0FC_ZoUR.js";const n=()=>{const{toastSuccess:t,toastError:s,toastInfo:a,showToast:r}=e();return{show:(o,c="info")=>{switch(c){case"success":t(o);break;case"error":s(o);break;case"info":a(o);break;default:r(o,"default");break}}}};export{n as N};
-//# sourceMappingURL=NotificationsService-6L-rsrZK.js.map
